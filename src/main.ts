867904d63fe326c8fe64f4de6b0c385fc1392fa5
StefanGuelland <github@sguelland.de>
2022-12-16 17:23:18 +0000

    add split unit example

--- src/main.ts
+++ src/main.ts
@@ -4,37 +4,38 @@ const freeAtHome = new FreeAtHome();
 freeAtHome.activateSignalHandling();
 
 async function main() {
-  const virtualSwitch = await freeAtHome.createSwitchingActuatorDevice("123switch", "Virtual Switch");
-  virtualSwitch.setAutoKeepAlive(true);
-  virtualSwitch.isAutoConfirm = true;
-  virtualSwitch.on('isOnChanged', (value: boolean) => {
-    console.log("switch state is:", (value) ? "on" : "off");
-  });
+  {
+    const channel = await freeAtHome.createSplitUnitDevice("mySplitUniteId", "SplitUnite");
+    channel.setAutoKeepAlive(true);
 
-  const virtualDimming = await freeAtHome.createDimActuatorDevice("123Dim", "Virtual Dimming");
-  virtualDimming.setAutoKeepAlive(true);
-  virtualDimming.isAutoConfirm = true;
-  virtualDimming.on('isOnChanged', (value: boolean) => {
-    console.log("dimming state is:", (value) ? "on" : "off");
-  });
-  virtualDimming.on("absoluteValueChanged", (value: number) => {
-    console.log("dimming value is:", value );
-  });
+    channel.setAutoConfirm(true); // use this for devices without feedback
+
+    // use to set state from your device to free at home (for devices with feedback)
+    channel.setOn(true);
+    channel.setModeHeating();
+    channel.sendSetPointTemperature(20);
+
+    // react on events from free at home
+    channel.on("isOnChanged", (value: boolean) => {
+        console.log("set ", value ? "on" : "off");
+    });
+
+    channel.on("setPointTemperatureChanged", (value: number) => {
+        console.log("setTemperature:", value);
+    });
+
+    channel.on("setModeCooling", () => {
+      console.log("set cooling");
+    });
+
+    channel.on("setModeHeating", () => {
+        console.log("set heating");
+    });
+
+    channel.on("setModeAuto", () => {
+        console.log("set auto");
+    });
+  }
 }
 
 main();
-
-// Get notified about changes in the configuration of the add on
-//#################################################################################
-
-import {AddOn} from '@busch-jaeger/free-at-home';
-
-const metaData = AddOn.readMetaData();
-
-const addOn = new AddOn.AddOn(metaData.id);
-
-addOn.on("configurationChanged", (configuration: AddOn.Configuration) => {
-  console.log(configuration);
-});
-
-addOn.connectToConfiguration();
